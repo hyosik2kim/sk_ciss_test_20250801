c669d23d39b0f138486709750e9b39e81a3c2f37
김효식_외주( )//기타 <12507005@sksignet.com>
2025-08-11 15:18:18 +0900

    mode DB Front to Back 4(modify)

--- ciss-front-end/src/lib/pyApiClient.ts
+++ ciss-front-end/src/lib/pyApiClient.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_CISS_API_URL ?? "http://localhost:5000",
+  baseURL: process.env.NEXT_PUBLIC_CISS_PYTHON_URL ?? "http://localhost:5000",
   headers: { "Content-Type": "application/json" },
 });
 
